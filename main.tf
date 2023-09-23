f7d39e023995ecb027d990ceac52716b6c35cd34
elnino sec16 <elninosec16@outlook.com>
2023-09-23 00:01:05 +0000

    terraform tasks for this issue completed
    the documentation is also completed for this issue

--- main.tf
+++ main.tf
@@ -1,1 +1,25 @@
+#how to use random provider
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.5.1"
+    }
+  }
+}
 
+provider "random" {
+  # Configuration options
+}
+
+#random_string to generate random string
+resource "random_string" "bucket_name" {
+  length           = 16
+  special          = false
+}
+
+#To display the random_string using TF output command
+#output command for this section could be either id or result 
+output "random_bucket_name_result" {
+  value = random_string.bucket_name.result
+}
