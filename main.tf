c184af9d4895b450304942e419fb6245c7819e40
elninosec16 <elninosec16@outlook.com>
2023-09-22 21:51:57 -0400

    complete all the tasks for this issue (#16)
    all the tasks were successfully completed

--- main.tf
+++ main.tf
@@ -1,25 +1,50 @@
 #how to use random provider
 terraform {
   required_providers {
+    #TF random provider config
+    # https://registry.terraform.io/providers/hashicorp/random/latest/docs
     random = {
       source = "hashicorp/random"
       version = "3.5.1"
     }
+    #AWS Provider for the S3 config to work
+    # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.17.0"
+    } 
   }
 }
+#**Important provide note**: be sure to perform `terraform init -upgrade` when adding new provider config to the current TF config.
 
 provider "random" {
   # Configuration options
 }
 
 #random_string to generate random string
+# https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
 resource "random_string" "bucket_name" {
-  length           = 16
-  special          = false
+  lower     = true
+  upper     = false
+  length    = 32
+  special   = false
 }
 
 #To display the random_string using TF output command
 #output command for this section could be either id or result 
-output "random_bucket_name_result" {
+output "random_bucket_name" {
   value = random_string.bucket_name.result
+}
+
+#S3 configuration
+resource "aws_s3_bucket" "s3-btcamp-tst" {
+  #S3 name restriction
+  # https://docs.aws.amazon.com/AmazonS3/latest/userguide/BucketRestrictions.html
+  bucket = random_string.bucket_name.result
+
+  tags = {
+    Name        = "s3-tf-bootcamp-1"
+    Environment = "bootcamp-2023"
+    Terraform	  = "True"
+  }
 }
