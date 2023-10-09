88e77d4fc96cd9592b2e63d43ca7f9f0fe99454d
El Nino Sec 16 <elninosec16@outlook.com>
2023-10-08 21:39:25 -0400

    all the tasks for this issue have been successfully completed
    tf-assests-upload_37

--- modules/terrahouse_aws/resource_storage.tf
+++ modules/terrahouse_aws/resource_storage.tf
@@ -73,6 +73,19 @@ resource "aws_s3_object" "error_object" {
   }
 }
 
+resource "aws_s3_object" "upload_assets" {
+  for_each      = fileset(var.assets_path,"*.{jpg,png,gif}")
+  bucket        = aws_s3_bucket.s3-btcamp-tst.bucket
+  key           = "assets/${each.key}" 
+  source        = "${var.assets_path}/${each.key}"
+  #content_type = "text.html"
+  etag          = filemd5("${var.assets_path}/${each.key}")
+  lifecycle {
+    replace_triggered_by = [terraform_data.content_version.output]
+    ignore_changes       = [etag]
+  }
+}
+
 #S3 local origin ID configuration:
 locals {
   s3_origin_id = "myS3Origin"
