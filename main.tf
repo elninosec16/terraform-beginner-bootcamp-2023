382ac7998738385d51ae5417f6536024f2375257
elnino sec16 <elninosec16@outlook.com>
2023-10-13 02:42:34 +0000

    48-terratowns-code-test
    the validation tests were successfully completed.

--- main.tf
+++ main.tf
@@ -1,27 +1,4 @@
 
-#S3 configuration
-#resource "aws_s3_bucket" "s3-btcamp-tst" {
-#  #S3 name restriction
-#  # https://docs.aws.amazon.com/AmazonS3/latest/userguide/BucketRestrictions.html
-#  bucket = var.s3_bucket_name
-#
-#}
-
-# module "terraform_aws" {
-#   source = "./modules/terrahouse_aws"
-#   
-#   
-#   user_uuid           = var.user_uuid
-#   s3_bucket_name      = var.s3_bucket_name
-#   terraform           = var.terraform
-#   project_name        = var.project_name
-#   index_html_filepath = var.index_html_filepath
-#   error_html_filepath = var.error_html_filepath
-#   content_version     = var.content_version
-#   assets_path         = var.assets_path
-# 
-# }
-
 terraform {
   required_providers {
     terratowns = {
@@ -32,21 +9,45 @@ terraform {
 }
 
 provider "terratowns" {
-  endpoint = "http://localhost:4567/api"
-  user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
-  token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+  endpoint  = var.terratowns_endpoint
+  user_uuid = var.teacherseat_user_uuid 
+  token     = var.terratowns_access_code
+}
+
+
+#S3 configuration
+resource "aws_s3_bucket" "s3-btcamp-tst" {
+  #S3 name restriction
+  # https://docs.aws.amazon.com/AmazonS3/latest/userguide/BucketRestrictions.html
+  bucket = var.s3_bucket_name
+
+}
+
+module "terrahouse_aws" {
+  source = "/workspace/terraform-beginner-bootcamp-2023/modules/terrahouse_aws"
+  teacherseat_user_uuid = var.teacherseat_user_uuid
+  terratowns_access_code = var.terratowns_access_code
+  terratowns_endpoint = var.terratowns_endpoint
+  s3_bucket_name      = var.s3_bucket_name
+  terraform           = var.terraform
+  project_name        = var.project_name
+  index_html_filepath = var.index_html_filepath
+  error_html_filepath = var.error_html_filepath
+  content_version     = var.content_version
+  assets_path         = var.assets_path
+
 }
 
 resource "terratowns_home" "home" {
   name = "How to play Arcanum in 2023"
   description = <<DESCRIPTION
-Arcanum is a game from 2001 that shipped with alot of bugs.
-Modders have removed all the originals making this game really fun
-to play (despite that old look graphics). This is my guide that will
-show you how to play arcanum without spoiling the plot.
+Los Peques is a 3D animation trolls cartoons created in 2001 made in the Providence of Neuquen Argentina. 
+Los Peques town was created in some place on the Andean Patagonian Montain Range of Neuquen. 
+Los Peques' houses are called peque rukas. The main Peques caracters are Nino & Chicho. 
+Los Peques is one of the best cartoons created in South America, and really fun to watch. 
 DESCRIPTION
-  #domain_name = module.terrahouse_aws.cloudfront_url
-  domain_name = "3fdq3gz.cloudfront.net"
-  town = "gamers-grotto"
-  content_version = 1
+  domain_name = module.terrahouse_aws.cloudfront_distribution_url
+  #domain_name = "3fdq3g456z.cloudfront.net"
+  town = "missingo"
+  content_version = var.content_version
 }
